00e8fb5ce58cf8aa9872621d0386e4a49c3f6576
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-11-20 22:56:23 +0200

    Update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+## Capturi de ecran
+
+- **Listare contacte**: (https://monosnap.com/file/FX3zdLa5hpyROBOM3QtbX2YqXAqQky)
+- **Contact după ID**: (https://monosnap.com/file/VdH2aH27vobdRztHHDCYUNNjqpU1re)
+- **Adăugare contact**: (https://monosnap.com/file/420O5t3Z2oW2123K4TFO1UfFpiJ1RH)
+- **Ștergere contact**: (https://monosnap.com/file/ZNKt7ItlsexXJkvNSTKSDq6Iv2FPF8)
--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const argv = require('yargs').argv;
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // Afișează lista completă de contacte
+      
       console.table(await listContacts());
       break;
 
     case 'get':
-      // Afișează un contact după id
+     
       console.log(await getContactById(id));
       break;
 
     case 'add':
-      // Adaugă un contact nou
+      
       console.log(await addContact(name, email, phone));
       break;
 
     case 'remove':
-      // Șterge un contact după id
+     
       console.log(await removeContact(id));
       break;
 
@@ -27,6 +27,5 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn('Unknown action!');
   }
 }
-
 
 invokeAction(argv);
