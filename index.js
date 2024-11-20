14cef8f9d1f77dd595910830660c874c16396d13
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-11-20 22:44:32 +0200

    Update modify contacts.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-console.log('Hello, World!');
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+const argv = require('yargs').argv; 
 
-const argv = require("yargs").argv;
-const contacts = require("./contacts");
-
-function invokeAction({ action, id, name, email, phone }) {
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      contacts.listContacts();
+    case 'list':
+      // Afișează lista completă de contacte
+      console.table(await listContacts());
       break;
 
-    case "get":
-      contacts.getContactById(id);
+    case 'get':
+      // Afișează un contact după id
+      console.log(await getContactById(id));
       break;
 
-    case "add":
-      contacts.addContact(name, email, phone);
+    case 'add':
+      // Adaugă un contact nou
+      console.log(await addContact(name, email, phone));
       break;
 
-    case "remove":
-      contacts.removeContact(id);
+    case 'remove':
+      // Șterge un contact după id
+      console.log(await removeContact(id));
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('Unknown action!');
   }
 }
+
 
 invokeAction(argv);
